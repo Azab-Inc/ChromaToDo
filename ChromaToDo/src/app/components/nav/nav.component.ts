25206d343963de5a32f14d46563f35b0935aec6a
AZ <47402666+realazaber@users.noreply.github.com>
2023-08-25 21:21:49 +0200

    Base nav

---
+++ ChromaToDo/src/app/components/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav.component.html',
+  styles: [
+  ]
+})
+export class NavComponent {
+
+}
