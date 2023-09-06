e10258fae895a7f196bfe07cf2e8e08a8241a27b
AZ <47402666+realazaber@users.noreply.github.com>
2023-09-06 19:26:07 +0200

    Base todo component

---
+++ ChromaToDo/src/app/components/todo/todo.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Todo } from 'src/app/todo';
+import { TodoService } from 'src/app/services/todo.service';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo.component.html',
+})
+export class TodoComponent {
+  @Input() todo: Todo | null = null;
+
+  constructor(private todoService: TodoService) {}
+
+  deleteSelf(): void {
+    if (this.todo != null) {
+      this.todoService.deleteTodo(this.todo);
+      alert(`Deleted ${this.todo.title}`);
+    }
+  }
+}
