70c030155c1d95dfca163477b3466bff2e9e07d0
AZ <47402666+realazaber@users.noreply.github.com>
2023-09-06 19:25:58 +0200

    Base category menu component

---
+++ ChromaToDo/src/app/components/category-menu/category-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-category-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './category-menu.component.html',
+  styles: [
+  ]
+})
+export class CategoryMenuComponent {
+
+}
