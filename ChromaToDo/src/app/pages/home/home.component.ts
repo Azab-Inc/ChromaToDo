654d34b97e48209e687d113b4a380faf5986bbce
AZ <47402666+realazaber@users.noreply.github.com>
2023-09-06 19:26:15 +0200

    Base home page

--- ChromaToDo/src/app/pages/home/home.component.ts
+++ ChromaToDo/src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
+import { Todo } from 'src/app/todo';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  todos: Todo[] | null = null;
 
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.todos = this.todoService.getAllTodos();
+  }
 }
--- ChromaToDo/src/app/pages/home/home.module.ts
+++ ChromaToDo/src/app/pages/home/home.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { TodoComponent } from 'src/app/components/todo/todo.component';
+import { AddTodoComponent } from 'src/app/components/add-todo/add-todo.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, TodoComponent, AddTodoComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
