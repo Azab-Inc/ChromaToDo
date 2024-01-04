96908a58d04160ce1838a5be7118400f2af3e321
AZ <47402666+realazaber@users.noreply.github.com>
2024-01-04 18:01:18 +0100

    Update tailwind.config.js

--- ChromaToDo/tailwind.config.js
+++ ChromaToDo/tailwind.config.js
@@ -3,13 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-      colors: {
+      colors: (theme) => ({
+        mainColor: theme("userColor", "#3e86f4"),
         mainR: "#dc3022",
         mainG: "#249b46",
         mainY: "#fcc92c",
         mainB: "#1274e9",
         accent: "#73787d",
-      },
+      }),
     },
   },
   plugins: [],
